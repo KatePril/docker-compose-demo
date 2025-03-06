b5e5e6bbcb3bd0a04c9d75e67a884587ff1b4471
Kateryna Prylutska <prylutska.kateryna@lll.kpi.ua>
2025-03-06 18:33:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-import dotenv
-import psycopg2
-import flask
+from dotenv import load_dotenv
+from psycopg2 import connect
+from flask import Flask, request, jsonify
 import os
 
-dotenv.load_dotenv()
+load_dotenv()
 POSTGRES_USER = os.getenv("POSTGRES_USER")
 POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
 POSTGRES_DB = os.getenv("POSTGRES_DB")
+POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+POSTGRES_PORT = os.getenv("POSTGRES_PORT")
 
-POSTGRES_HOST = "postgres"
-POSTGRES_PORT = "5432"
-app = flask.Flask(__name__)
-conn = psycopg2.connect(database=POSTGRES_DB,
+app = Flask(__name__)
+conn =  connect(database=POSTGRES_DB,
                         user=POSTGRES_USER,
                         password=POSTGRES_PASSWORD,
                         host=POSTGRES_HOST,
@@ -21,23 +21,27 @@ cursor = conn.cursor()
 @app.route('/', methods=['GET'])
 def get_contacts():
     cursor.execute("SELECT * FROM contacts")
-    rows = cursor.fetchall()  # Fetch all rows from the query result
-
-    return flask.jsonify({"contacts": rows})
+    rows = cursor.fetchall()
+    return jsonify({"contacts": rows})
 
 @app.route('/', methods=['POST'])
 def get_contact():
-    data = flask.request.get_json()
+    data = request.get_json()
 
     if not data:
-        return flask.jsonify({"error": "No input data provided"}), 400
+        return jsonify({"error": "No input data provided"}), 400
     name = data.get("name")
+    if not name:
+        return jsonify({"error": "name is a mandatory parameter"}), 400
+
     phone_number = data.get("phone_number")
     email = data.get("email")
+
     query = "INSERT INTO contacts (name, phone_number, email) VALUES (%s, %s, %s)"
     res = cursor.execute(query, (name, phone_number, email))
     conn.commit()
-    return flask.jsonify({"test": str(res)})
+
+    return jsonify({"test": str(res)})
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8080, debug=True)
